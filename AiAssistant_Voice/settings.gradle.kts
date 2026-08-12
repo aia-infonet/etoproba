bece31c4aee163947d2b4bc7fa387828b7389a12
aia-infonet <infonet@aia.ua>
2026-08-12 13:30:24 +0300

    Add files via upload

---
+++ AiAssistant_Voice/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.alphacephei.com/repository/maven-public/") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.alphacephei.com/repository/maven-public/") }
+    }
+}
+
+rootProject.name = "AiAssistant_Voice"
+include(":app")
